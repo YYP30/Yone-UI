268e3c14673e19539fab951554c7eb1cc9144249
YYP-Allen <1519327651@qq.com>
2020-07-28 19:56:32 +0800

    使用 npm test 与 npm run dev-test 进行自动测试

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ chai.use(spies)
 const expect = chai.expect
 // 单元测试
 {
+  // 测试按钮含有 icon
   const Constructor = Vue.extend(Button)
   const vm = new Constructor({
     propsData: {
@@ -36,6 +37,7 @@ const expect = chai.expect
 }
 
 {
+  // 测试按钮含有 loading
   const Constructor = Vue.extend(Button)
   const vm = new Constructor({
     propsData: {
